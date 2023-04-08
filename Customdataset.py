50e9905e1b041acfa91780d5d32fb1a26ec5fd32
SunCreation <cake0103@nate.com>
2023-04-08 03:42:59 +0000

    feat: CustomDataset

---
+++ Customdataset.py
@@ -0,0 +1,72 @@
+from torch.utils.data import Dataset
+from torch.nn import functional as F
+import random
+import torch
+import numpy as np
+import pandas as pd
+import soundfile as sf
+
+
+
+N_SAMPLES = 480000
+
+"""기쁨happy, 놀람surprise, 분노angry, 중립neutral, 혐오disgust, 공포fear, 슬픔sad"""
+
+def pad_or_trim(array, length: int = N_SAMPLES, *, axis: int = -1):
+    """
+    Pad or trim the audio array to N_SAMPLES, as expected by the encoder.
+    """
+    if torch.is_tensor(array):
+        if array.shape[axis] > length:
+            array = array.index_select(
+                dim=axis, index=torch.arange(length, device=array.device)
+            )
+
+        if array.shape[axis] < length:
+            pad_widths = [(0, 0)] * array.ndim
+            pad_widths[axis] = (0, length - array.shape[axis])
+            array = F.pad(array, [pad for sizes in pad_widths[::-1] for pad in sizes])
+    else:
+        if array.shape[axis] > length:
+            array = array.take(indices=range(length), axis=axis)
+
+        if array.shape[axis] < length:
+            pad_widths = [(0, 0)] * array.ndim
+            pad_widths[axis] = (0, length - array.shape[axis])
+            array = np.pad(array, pad_widths)
+
+    return array
+
+
+class CustomDataset(Dataset):
+    def __init__(self, csv_path, processor=None):
+        self.data = pd.read_csv(csv_path, header=[0, 1])
+        self.text_data = self.data['text_data'][' '].values
+        self.wav_dir = self.data['wav_dir'][' '].values
+        self.dic = {'happy': 0, 'surprise': 1, 'angry': 2, 'neutral': 3, 'disqust': 4, 'fear': 5, 'sad': 6}
+        self.labels = self.data['Total Evaluation']['Emotion'].values
+        self.processor = processor
+        self.shuffle = np.arange(len(self))
+        np.random.shuffle(self.shuffle)
+      
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, idx):
+        idx = self.shuffle[idx]
+        if ';' in self.labels[idx]:
+            text_label = self.labels[idx].split(';')[random.choice([0,1])]
+        else:
+            text_label = self.labels[idx]
+
+        if self.processor:
+            audio_input, sample_rate = sf.read(self.wav_dir[idx])
+            audio_input = pad_or_trim(audio_input)
+            audio_input_values = self.processor(audio_input, sampling_rate=sample_rate, return_tensors="pt").input_values.squeeze(0)
+
+            return self.text_data[idx], audio_input_values, self.dic[text_label]
+        
+        else:
+            return self.text_data[idx], self.dic[text_label]
+
+
